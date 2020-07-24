709493a7a0ac88802eee563c02fff3927ae9a496
lin <mr03web@foxmail.com>
2020-07-24 18:11:35 +0800

    AddTwoNumbers

---
+++ src/2/AddTwoNumbers.ts
@@ -0,0 +1,15 @@
+/**
+ * @Date 2020-07-24 17:57:43
+ * @Remark AddTwoNumbers
+ */
+
+import ListNode from "./ListNode";
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null,
+): ListNode | null {
+  return null;
+}
+
+export default addTwoNumbers;
